5509fa0a9c4ac0ace27fd93ba5e1bfe9fb61bf93
ROAA-14 <roro3066@gmail.com>
2024-12-17 01:01:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const validUsername = "admin";
+const validPassword = "12345";
+
+const loginForm = document.getElementById("loginForm");
+const errorMessage = document.getElementById("error-message");
+
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === validUsername && password === validPassword) {
+        alert("Login successful!");
+        window.location.href = "welcome.html";
+    } else {
+        errorMessage.style.display = "block";
+    }
+});
